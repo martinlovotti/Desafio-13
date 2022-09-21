1488dbb5191215a1d77dea9d10e4da9193cae0a8
Martin Fernandez <martin0022f@gmail.com>
2022-09-21 18:17:53 -0300

    Desafio 13

---
+++ README.md
@@ -0,0 +1,7 @@
+# MARTIN FERNANDEZ- Curso CODERHOUSE Backend
+
+Desafio 13
+
+eCommerce Backend
+
+2022 - Curso de Backend en CoderHouse
---
+++ routes/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+
+const products = require("./productRouter");
+const productsList = require("./productListRouter");
+const home = require("./homeRouter");
+const homeFaker = require("./homeFakerRouter");
+const productosFaker = require("./productosFakerRouter");
+const login = require("./loginRouter");
+const loginError = require("./loginErrorRouter");
+const register = require("./registerRouter");;
+const logout = require("./logoutRouter");
+
+//middlewares
+router.use("/productos", products);
+router.use("/lista-productos", productsList);
+router.use("/", home);
+router.use("/login", login);
+router.use("/loginerror", loginError);
+router.use("/register", register);
+router.use("/logout", logout);
+router.use("/api/productos-test", homeFaker);
+router.use("/api/faker-list", productosFaker);
+
+
+module.exports = router;
